0005f31a1ecc8270f683d79c72116b9eb7ea9543
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-06-28 22:36:39 +0200

    Create animation_basic.h

---
+++ animation_basic.h
@@ -0,0 +1,7 @@
+#ifndef ANIMATION_BASIC_H
+#define ANIMATION_BASIC_H
+
+char * prepareTheAnimation01WithFrame ( unsigned char frame );
+char * prepareTheAnimation02WithFrame ( unsigned char frame );
+
+#endif // ANIMATION_BASIC_H
